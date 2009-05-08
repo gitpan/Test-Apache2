3b2a598b62738a4a69ba2fa20d06b07a135cf546
加藤 和良 <kato.kazuyoshi@gmail.com>
2009-05-08 19:37:10 +0000

    Import of KZYS/Test-Apache2-0.02 from CPAN.
    gitpan-cpan-distribution: Test-Apache2
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KZYS/Test-Apache2-0.02.tar.gz
    gitpan-cpan-author:       KZYS
    gitpan-cpan-maturity:     released

--- lib/Test/Apache2.pm
+++ lib/Test/Apache2.pm
@@ -3,23 +3,10 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 use Test::Apache2::Server;
-
-sub import {
-    {
-        package Apache2::ServerUtil;
-
-        sub server_root {
-            '';
-        }
-
-        sub restart_count {
-            0;
-        }
-    }
-}
+use Test::Apache2::Override;
 
 1;
 __END__
